3ed7c7a9b150367fda5eb5112e84a8042f81e52c
= <=>
2019-07-10 14:05:45 +0200

    add integration test structure

---
+++ test/integration/validate_test.go
@@ -0,0 +1,13 @@
+package integration_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Validate", func() {
+
+	It("TODO", func() {
+		Expect(true).To(BeTrue())
+	})
+})
---
+++ test/integration/version_test.go
@@ -0,0 +1,13 @@
+package integration_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Version", func() {
+
+	It("TODO", func() {
+		Expect(true).To(BeTrue())
+	})
+})
